43d9e210082b98c3b17bf0f29222e154e2b13ce8
Ruben <ruben.maldonado05@utrgv.edu>
2023-12-05 10:11:13 -0600

    Added My Template

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-# First commits
+# Import libraries and packages, they contain additional functions.
+from flask import Flask, render_template
+# Create web application using Flask
+app = Flask(__name__)
+# Create our routes
+@app.route('/')
+def home():
+ return render_template('index.html')
+# Start the web application
+app.run(
+ debug=True
+)
